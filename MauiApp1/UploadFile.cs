5ae1d684fd5c2afeab734986dc3fc695f62165ea
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2025-08-30 17:33:54 +0200

    Update API use UploadFile

---
+++ MauiApp1/UploadFile.cs
@@ -0,0 +1,8 @@
+﻿public class UploadFile
+{ 
+    public required Guid uuid { get; set; }
+    public required string filePath { get; set; }
+    public required string mimeType { get; set; }
+    public required string source { get; set; }
+
+}
