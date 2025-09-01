4b913a8da3be25f778330c7170dc14ffe03d335e
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2025-09-02 00:08:34 +0200

    Update App

--- MauiApp1/SignIn.xaml.cs
+++ MauiApp1/SignIn.xaml.cs
@@ -28,36 +28,35 @@ public partial class SignInPage : ContentPage
     }
     private async void OnSignInClicked(object sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync("mainpage");
+        var values = new Dictionary<string, string> {
+            { "emailAddress", EmailAddress },
+            { "password", Password },
+        };
 
-        //var values = new Dictionary<string, string> {
-        //    { "emailAddress", EmailAddress },
-        //    { "password", Password },
-        //};
+        var response = await _authenticate.newSession(values);
+        var jsonResponse = JsonSerializer.Deserialize<NewSessionResponse>(response);
 
-        //var response = await _authenticate.newSession(values);
-        //var jsonResponse = JsonSerializer.Deserialize<NewSessionResponse>(response);
+        if (jsonResponse?.sessionId != null)
+        {
+            _authenticate.setSessionID(jsonResponse.sessionId);
+        }
 
-        //if (jsonResponse?.sessionId != null)
-        //{
-        //    _authenticate.setSessionID(jsonResponse.sessionId);
-        //}
-
-        //if (jsonResponse?.user != null)
-        //{
-        //    if (jsonResponse?.user?.id != null)
-        //    {
-        //        await _authenticate.setCurrentUser(jsonResponse.user);
-        //        await Shell.Current.GoToAsync("mainpage");
-        //    }
-        //    else
-        //    {
-        //        await DisplayAlert("Login", string.Concat(jsonResponse.user.errors), "OK");
-        //    }
-        //} else
-        //{
-        //    await DisplayAlert("Login", string.Concat(jsonResponse.message), "OK");
-        //}
+        if (jsonResponse?.user != null)
+        {
+            if (jsonResponse?.user?.id != null)
+            {
+                await _authenticate.setCurrentUser(jsonResponse.user);
+                await Shell.Current.GoToAsync("mainpage");
+            }
+            else
+            {
+                await DisplayAlert("Login", string.Concat(jsonResponse.user.errors), "OK");
+            }
+        }
+        else
+        {
+            await DisplayAlert("Login", string.Concat(jsonResponse.message), "OK");
+        }
     }
     private async void OnEmailAddressCompleted(object sender, EventArgs e)
     {
