c24a7e2887b87efc7de4acae640023ae281c51f7
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2025-09-10 17:41:00 +0200

    Update App

---
+++ MauiApp1/Upload.cs
@@ -0,0 +1,15 @@
+﻿public class Upload
+{
+    public int? id { get; set; }
+    public Guid? uuid { get; set; }
+
+    // public ImageFile[] imageFiles { get; set; }
+
+    // public PdfFile[] pdfFiles { get; set; }
+
+    // public TextFile[] textFiles { get; set; }
+
+    // public AudioFile[] audioFiles { get; set; }
+
+    // public VideoFile[] videoFiles { get; set; }
+}
---
+++ MauiApp1/UploadFolder.cs
@@ -0,0 +1,6 @@
+﻿public class UploadFolder
+{
+    public required string Path { get; set; }
+    public required string Type { get; set; }
+
+}
