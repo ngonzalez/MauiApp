98b633a0277d287cd6ceb36e2d5dd623522f3dc7
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2025-09-13 23:26:45 +0200

    Update App

---
+++ MauiApp1/PdfFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class PdfFile
+{
+    public int id { get; set; }
+    public Folder folder { get; set; }
+    public string fileName { get; set; }
+    public string fileUrl { get; set; }
+    public string? dataUrl { get; set; }
+    public string? mimeType { get; set; }
+    public string? formatInfo { get; set; }
+
+}
