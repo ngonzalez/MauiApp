e83f2e73afcaa9435357ef375d05efb0f4e006aa
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2025-11-09 16:58:55 +0100

    ImageFile remove thumb

--- MauiApp1/ImageFile.cs
+++ MauiApp1/ImageFile.cs
@@ -4,7 +4,6 @@
     public Folder folder { get; set; }
     public string fileName { get; set; }
     public string fileUrl { get; set; }
-    public string thumbUrl { get; set; }
     public string? dataUrl { get; set; }
     public string? mimeType { get; set; }
     public string? formatInfo { get; set; }
