3f459e6929e8598618ec22b5641d9c730d529ad9
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2026-04-04 21:00:12 +0200

    Update App

--- MauiApp1/DisplayPage.xaml.cs
+++ MauiApp1/DisplayPage.xaml.cs
@@ -22,12 +22,6 @@ public class CollectionIds
     public string[] type { get; set; }
 }
 
-public class CollectionTypeIds
-{
-    public string id { get; set; }
-
-    public string type { get; set; }
-}
 
 public class VideoStreamResponse
 {
@@ -51,6 +45,8 @@ public partial class DisplayPage : ContentPage
 
     private Folder _folder;
     public List<PickerOption> ImageFileActionPickerOptions { get; set; }
+    public List<PickerOption> VideoFileActionPickerOptions { get; set; }
+    public List<PickerOption> AudioFileActionPickerOptions { get; set; }
 
     public ObservableCollection<ImageFile> ImageFiles { get; set; }
     public ObservableCollection<ImageFile> SelectedImageFiles { get; set; }
@@ -133,6 +129,20 @@ public partial class DisplayPage : ContentPage
         refreshImageFilesButton.Clicked += new EventHandler(refreshImageFilesButtonClicked);
         PopulateImageFilesActionPicker();
 
+        // Video File Actions
+        selectAllVideoFiles.Clicked += new EventHandler(selectAllVideoFilesButtonClicked);
+        VideoFileActionPicker.SelectedIndexChanged += new EventHandler(VideoFileActionPickerOnSelectedIndexChanged);
+        selectedVideoFilesCountLabel.Text = "No Video Files selected";
+        refreshVideoFilesButton.Clicked += new EventHandler(refreshVideoFilesButtonClicked);
+        PopulateVideoFilesActionPicker();
+
+        // Audio File Actions
+        selectAllAudioFiles.Clicked += new EventHandler(selectAllAudioFilesButtonClicked);
+        AudioFileActionPicker.SelectedIndexChanged += new EventHandler(AudioFileActionPickerOnSelectedIndexChanged);
+        selectedAudioFilesCountLabel.Text = "No Audio Files selected";
+        refreshAudioFilesButton.Clicked += new EventHandler(refreshAudioFilesButtonClicked);
+        PopulateAudioFilesActionPicker();
+
         // Get media files from backend
         getUploads();
     }
@@ -195,6 +205,7 @@ public partial class DisplayPage : ContentPage
         videoFilesCount.Text = Convert.ToString(VideoFiles.Count()) + " " + videoFileLabel;
         GridVideoFiles.IsVisible = VideoFiles.Count() > 0;
         GridVideoFilesDetails.IsVisible = VideoFiles.Count() > 0;
+        videoFilesCountLabel.Text = Convert.ToString(VideoFiles.Count() + " " + videoFileLabel);
 
         // AudioFile
         AudioFiles = new ObservableCollection<AudioFile> { };
@@ -220,6 +231,7 @@ public partial class DisplayPage : ContentPage
         audioFilesCount.Text = Convert.ToString(AudioFiles.Count()) + " " + audioFileLabel;
         GridAudioFiles.IsVisible = AudioFiles.Count() > 0;
         GridAudioFilesDetails.IsVisible = AudioFiles.Count() > 0;
+        audioFilesCountLabel.Text = Convert.ToString(AudioFiles.Count() + " " + audioFileLabel);
 
         // PdfFile
         PdfFiles = new ObservableCollection<PdfFile> { };
@@ -367,13 +379,14 @@ public partial class DisplayPage : ContentPage
 
             if (action == "Delete")
             {
-                var folderIds = new CollectionIds
+
+                var imageFileIds = new CollectionIds
                 {
                     id = ids.ToArray(),
                     type = ["ImageFile"]
                 };
 
-                byte[] body = JsonSerializer.SerializeToUtf8Bytes(folderIds);
+                byte[] body = JsonSerializer.SerializeToUtf8Bytes(imageFileIds);
 
                 if (selectedOption.Name == "Delete Image Files")
 
@@ -690,12 +703,225 @@ public partial class DisplayPage : ContentPage
         }, 2000);
     }
 
+    public List<string> getSelectedVideoFileIds()
+    {
+        List<string> ids = new List<string>();
+        var rootViewsAndTheirDescendants = videoFilesCollectionView.GetVisualTreeDescendants();
+        foreach (VisualElement element in rootViewsAndTheirDescendants)
+        {
+            if (element is Microsoft.Maui.Controls.CheckBox)
+            {
+                CheckBox checkbox = (CheckBox)element;
+                if (checkbox.IsChecked)
+                {
+                    ids.Add(checkbox.ClassId);
+                }
+            }
+        }
+        return ids;
+    }
+
+    public async void updateVideoFilesButtonClicked(object sender, EventArgs e)
+    {
+        List<string> ids = getSelectedVideoFileIds();
+        List<string> VideoFileNames = new List<string>();
+        foreach (VideoFile videoFile in VideoFiles)
+        {
+            if (ids.Contains(Convert.ToString(videoFile.id)))
+            {
+                VideoFileNames.Add(videoFile.fileName);
+            }
+        }
+
+        PickerOption selectedOption = null;
+        foreach (var pickerOption in VideoFileActionPickerOptions)
+        {
+            if (Convert.ToInt32(pickerOption.ID) == VideoFileActionPicker.SelectedIndex)
+            {
+                selectedOption = pickerOption;
+                break;
+            }
+        }
+
+        if (selectedOption != null)
+        {
+            string action = await DisplayActionSheet(selectedOption.Name, "Cancel", "Delete", String.Join("\n", VideoFileNames));
+
+            if (action == "Delete")
+            {
+                var videoFileIds = new CollectionIds
+                {
+                    id = ids.ToArray(),
+                    type = ["VideoFile"]
+                };
+
+                byte[] body = JsonSerializer.SerializeToUtf8Bytes(videoFileIds);
+
+                if (selectedOption.Name == "Delete Video Files")
+
+                {
+                    (int _statusCode, var response) = await _apiService.DeleteAttachments(body);
+                }
+
+                videoFilesSearchBar.Text = "";
+
+                VideoFileActionPicker.SelectedIndex = 0;
+
+                getUploads();
+
+                uncheckVideoFileCheckBoxes();
+
+                updateVideoFilesActionButton();
+            }
+        }
+    }
+
+    public void refreshVideoFilesButtonClicked(object sender, EventArgs e)
+    {
+        videoFilesSearchBar.Text = "";
+
+        getUploads();
+
+        updateVideoFilesActionButton();
+    }
+
+
+    private void VideoFileActionPickerOnSelectedIndexChanged(object sender, EventArgs e)
+    {
+        updateVideoFilesActionButton();
+    }
+
+    public void selectAllVideoFilesButtonClicked(object sender, EventArgs e)
+    {
+        Button selectAllFolders = (Button)sender;
+
+        toggleVideoFilesCheckBoxes();
+
+        updateVideoFilesActionButton();
+    }
+    private void PopulateVideoFilesActionPicker()
+    {
+        VideoFileActionPickerOptions = new List<PickerOption>
+            {
+                new PickerOption { ID = "0", Name = "" },
+                new PickerOption { ID = "1", Name = "Delete Video Files" },
+            };
+
+        foreach (var pickerOption in VideoFileActionPickerOptions)
+        {
+            VideoFileActionPicker.Items.Add(pickerOption.Name);
+        }
+    }
+
+    public void uncheckVideoFileCheckBoxes()
+    {
+        var rootViewsAndTheirDescendants = videoFilesCollectionView.GetVisualTreeDescendants();
+
+        foreach (VisualElement element in rootViewsAndTheirDescendants)
+        {
+            if (element is Microsoft.Maui.Controls.CheckBox)
+            {
+                CheckBox checkbox = (CheckBox)element;
+                checkbox.IsChecked = false;
+            }
+        }
+    }
+
+    public void toggleVideoFilesCheckBoxes()
+    {
+        var rootViewsAndTheirDescendants = videoFilesCollectionView.GetVisualTreeDescendants();
+
+        bool allChecked = true;
+        foreach (VisualElement element in rootViewsAndTheirDescendants)
+        {
+            if (element is Microsoft.Maui.Controls.CheckBox)
+            {
+                CheckBox checkbox = (CheckBox)element;
+                allChecked = checkbox.IsChecked;
+            }
+        }
+        foreach (VisualElement element in rootViewsAndTheirDescendants)
+        {
+            if (element is Microsoft.Maui.Controls.CheckBox)
+            {
+                CheckBox checkbox = (CheckBox)element;
+                checkbox.IsChecked = !allChecked;
+            }
+        }
+    }
+
+    public void videoFileCheckedChanged(object sender, EventArgs e)
+    {
+        CheckBox selectFolder = (CheckBox)sender;
+
+        updateVideoFilesActionButton();
+    }
+
+    public async void updateVideoFilesActionButton()
+    {
+        var rootViewsAndTheirDescendants = videoFilesCollectionView.GetVisualTreeDescendants();
+
+        int videoFilesCount = 0;
+        foreach (VisualElement element in rootViewsAndTheirDescendants)
+        {
+            if (element is Microsoft.Maui.Controls.CheckBox)
+            {
+                CheckBox checkbox = (CheckBox)element;
+                if (checkbox.IsChecked)
+                {
+                    videoFilesCount++;
+                }
+            }
+        }
+
+        if (videoFilesCount > 0)
+        {
+            string videoFilesLabel = videoFilesCount == 1 ? "Video File" : "Video Files";
+            selectedVideoFilesCountLabel.Text = Convert.ToString(videoFilesCount) + " " + videoFilesLabel + " selected";
+        }
+        else
+        {
+            selectedVideoFilesCountLabel.Text = "No Video Files selected";
+        }
+
+        PickerOption selectedOption = null;
+        foreach (var pickerOption in VideoFileActionPickerOptions)
+        {
+            if (Convert.ToInt32(pickerOption.ID) == VideoFileActionPicker.SelectedIndex)
+            {
+                selectedOption = pickerOption;
+                break;
+            }
+        }
+
+        if (videoFilesCount > 0 && (selectedOption != null && selectedOption.Name != ""))
+        {
+            updateVideoFilesButton.BackgroundColor = Colors.Orange;
+            updateVideoFilesButton.TextColor = Colors.Black;
+            updateVideoFilesButtonImage.Color = Colors.White;
+            VideoFileActionPicker.TextColor = Colors.FloralWhite;
+        }
+        else
+        {
+            updateVideoFilesButton.BackgroundColor = Colors.Black;
+            updateVideoFilesButton.TextColor = Colors.Gray;
+            updateVideoFilesButtonImage.Color = Colors.Gray;
+            VideoFileActionPicker.TextColor = Colors.Gray;
+        }
+    }
+
     public async void videoFilesSearchInputTextChanged(object sender, EventArgs e)
     {
         SearchBar searchBar = (SearchBar)sender;
         videoFilesCollectionView.ItemsSource = VideoFiles.Where(videoFile =>
             videoFile.fileName.Contains(searchBar.Text, StringComparison.OrdinalIgnoreCase)
         );
+
+        VideoFileActionPicker.SelectedIndex = 0;
+
+        uncheckVideoFileCheckBoxes();
+
+        updateVideoFilesActionButton();
     }
 
     public async void addVideoFileToSelectedItems(VideoFile videoFile)
@@ -929,21 +1155,215 @@ public partial class DisplayPage : ContentPage
         }, 2000);
     }
 
-    public async void VideoFileDeleteButton_Clicked(object sender, EventArgs e)
-    {
-        Button button = (Button)sender;
-        VideoFile videoFile = (VideoFile)button.BindingContext;
 
-        string action = await DisplayActionSheet("Delete Video File?", "Cancel", "Delete", string.Concat(videoFile.fileName));
-        if (action == "Delete") {
-            var videoFileIds = new CollectionTypeIds
+
+
+
+    public List<string> getSelectedAudioFileIds()
+    {
+        List<string> ids = new List<string>();
+        var rootViewsAndTheirDescendants = audioFilesCollectionView.GetVisualTreeDescendants();
+        foreach (VisualElement element in rootViewsAndTheirDescendants)
+        {
+            if (element is Microsoft.Maui.Controls.CheckBox)
             {
-                id = Convert.ToString(videoFile.id),
-                type = "VideoFile"
+                CheckBox checkbox = (CheckBox)element;
+                if (checkbox.IsChecked)
+                {
+                    ids.Add(checkbox.ClassId);
+                }
+            }
+        }
+        return ids;
+    }
+
+    public async void updateAudioFilesButtonClicked(object sender, EventArgs e)
+    {
+        List<string> ids = getSelectedImageFileIds();
+        List<string> AudioFileNames = new List<string>();
+        foreach (AudioFile audioFile in AudioFiles)
+        {
+            if (ids.Contains(Convert.ToString(audioFile.id)))
+            {
+                AudioFileNames.Add(audioFile.fileName);
+            }
+        }
+
+        PickerOption selectedOption = null;
+        foreach (var pickerOption in AudioFileActionPickerOptions)
+        {
+            if (Convert.ToInt32(pickerOption.ID) == AudioFileActionPicker.SelectedIndex)
+            {
+                selectedOption = pickerOption;
+                break;
+            }
+        }
+
+        if (selectedOption != null)
+        {
+            string action = await DisplayActionSheet(selectedOption.Name, "Cancel", "Delete", String.Join("\n", AudioFileNames));
+
+            if (action == "Delete")
+            {
+
+                var audioFileIds = new CollectionIds
+                {
+                    id = ids.ToArray(),
+                    type = ["AudioFile"]
+                };
+
+                byte[] body = JsonSerializer.SerializeToUtf8Bytes(audioFileIds);
+
+                if (selectedOption.Name == "Delete Audio Files")
+
+                {
+                    (int _statusCode, var response) = await _apiService.DeleteAttachments(body);
+                }
+
+                audioFilesSearchBar.Text = "";
+
+                AudioFileActionPicker.SelectedIndex = 0;
+
+                getUploads();
+
+                uncheckAudioFileCheckBoxes();
+
+                updateAudioFilesActionButton();
+            }
+        }
+    }
+
+    public void refreshAudioFilesButtonClicked(object sender, EventArgs e)
+    {
+        audioFilesSearchBar.Text = "";
+
+        getUploads();
+
+        updateAudioFilesActionButton();
+    }
+
+
+    private void AudioFileActionPickerOnSelectedIndexChanged(object sender, EventArgs e)
+    {
+        updateAudioFilesActionButton();
+    }
+
+    public void selectAllAudioFilesButtonClicked(object sender, EventArgs e)
+    {
+        Button selectAllFolders = (Button)sender;
+
+        toggleAudioFilesCheckBoxes();
+
+        updateAudioFilesActionButton();
+    }
+    private void PopulateAudioFilesActionPicker()
+    {
+        AudioFileActionPickerOptions = new List<PickerOption>
+            {
+                new PickerOption { ID = "0", Name = "" },
+                new PickerOption { ID = "1", Name = "Delete Image Files" },
             };
 
-            byte[] body = JsonSerializer.SerializeToUtf8Bytes(videoFileIds);
-            (int _statusCode, var response) = await _apiService.DeleteAttachments(body);
+        foreach (var pickerOption in AudioFileActionPickerOptions)
+        {
+            AudioFileActionPicker.Items.Add(pickerOption.Name);
+        }
+    }
+
+    public void uncheckAudioFileCheckBoxes()
+    {
+        var rootViewsAndTheirDescendants = imageFilesCollectionView.GetVisualTreeDescendants();
+
+        foreach (VisualElement element in rootViewsAndTheirDescendants)
+        {
+            if (element is Microsoft.Maui.Controls.CheckBox)
+            {
+                CheckBox checkbox = (CheckBox)element;
+                checkbox.IsChecked = false;
+            }
+        }
+    }
+
+    public void toggleAudioFilesCheckBoxes()
+    {
+        var rootViewsAndTheirDescendants = audioFilesCollectionView.GetVisualTreeDescendants();
+
+        bool allChecked = true;
+        foreach (VisualElement element in rootViewsAndTheirDescendants)
+        {
+            if (element is Microsoft.Maui.Controls.CheckBox)
+            {
+                CheckBox checkbox = (CheckBox)element;
+                allChecked = checkbox.IsChecked;
+            }
+        }
+        foreach (VisualElement element in rootViewsAndTheirDescendants)
+        {
+            if (element is Microsoft.Maui.Controls.CheckBox)
+            {
+                CheckBox checkbox = (CheckBox)element;
+                checkbox.IsChecked = !allChecked;
+            }
+        }
+    }
+
+    public void audioFileCheckedChanged(object sender, EventArgs e)
+    {
+        CheckBox selectFolder = (CheckBox)sender;
+
+        updateAudioFilesActionButton();
+    }
+
+    public async void updateAudioFilesActionButton()
+    {
+        var rootViewsAndTheirDescendants = audioFilesCollectionView.GetVisualTreeDescendants();
+
+        int audioFilesCount = 0;
+        foreach (VisualElement element in rootViewsAndTheirDescendants)
+        {
+            if (element is Microsoft.Maui.Controls.CheckBox)
+            {
+                CheckBox checkbox = (CheckBox)element;
+                if (checkbox.IsChecked)
+                {
+                    audioFilesCount++;
+                }
+            }
+        }
+
+        if (audioFilesCount > 0)
+        {
+            string audioFilesLabel = audioFilesCount == 1 ? "Audio File" : "Audio Files";
+            selectedAudioFilesCountLabel.Text = Convert.ToString(audioFilesCount) + " " + audioFilesLabel + " selected";
+        }
+        else
+        {
+            selectedImageFilesCountLabel.Text = "No Image Files selected";
+        }
+
+        PickerOption selectedOption = null;
+        foreach (var pickerOption in AudioFileActionPickerOptions)
+        {
+            if (Convert.ToInt32(pickerOption.ID) == AudioFileActionPicker.SelectedIndex)
+            {
+                selectedOption = pickerOption;
+                break;
+            }
+        }
+
+        if (audioFilesCount > 0 && (selectedOption != null && selectedOption.Name != ""))
+        {
+            updateAudioFilesButton.BackgroundColor = Colors.Orange;
+            updateAudioFilesButton.TextColor = Colors.Black;
+            updateAudioFilesButtonImage.Color = Colors.White;
+            AudioFileActionPicker.TextColor = Colors.FloralWhite;
+        }
+        else
+        {
+            updateAudioFilesButton.BackgroundColor = Colors.Black;
+            updateAudioFilesButton.TextColor = Colors.Gray;
+            updateAudioFilesButtonImage.Color = Colors.Gray;
+            AudioFileActionPicker.TextColor = Colors.Gray;
         }
     }
 
@@ -953,6 +1373,12 @@ public partial class DisplayPage : ContentPage
         audioFilesCollectionView.ItemsSource = AudioFiles.Where(audioFile =>
             audioFile.fileName.Contains(searchBar.Text, StringComparison.OrdinalIgnoreCase)
         );
+
+        AudioFileActionPicker.SelectedIndex = 0;
+
+        uncheckAudioFileCheckBoxes();
+
+        updateAudioFilesActionButton();
     }
 
     public async void addAudioFileToSelectedItems(AudioFile audioFile)
@@ -1175,25 +1601,6 @@ public partial class DisplayPage : ContentPage
         {
             AudioFileActionLabel.Text = "";
         }, 2000);
-    }
-
-    public async void AudioFileDeleteButton_Clicked(object sender, EventArgs e)
-    {
-        Button button = (Button)sender;
-        AudioFile audioFile = (AudioFile)button.BindingContext;
-
-        string action = await DisplayActionSheet("Delete Audio File?", "Cancel", "Delete", string.Concat(audioFile.fileName));
-        if (action == "Delete")
-        {
-            var audioFileIds = new CollectionTypeIds
-            {
-                id = Convert.ToString(audioFile.id),
-                type = "AudioFile"
-            };
-
-            byte[] body = JsonSerializer.SerializeToUtf8Bytes(audioFileIds);
-            (int _statusCode, var response) = await _apiService.DeleteAttachments(body);
-        }
     }
 
     public async void pdfFilesSearchInputTextChanged(object sender, EventArgs e)
